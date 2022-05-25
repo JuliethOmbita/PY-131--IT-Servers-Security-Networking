1bd27be9dbfa0796ba9b658fef6eed008797b546
JuliethOmbita <90491030+JuliethOmbita@users.noreply.github.com>
2022-05-25 12:45:21 -0400

    Update test_words.py

--- Lesson03-DjangoSpaceProject/projectSpace/IssLocation/test_words.py
+++ Lesson03-DjangoSpaceProject/projectSpace/IssLocation/test_words.py
@@ -13,10 +13,6 @@ class TestCalculatorMethods(unittest.TestCase):
         alg_vowels = resp['Algeria']['vowels']
         self.assertEqual(alg_vowels, 4, "Algeria has 4 vowels")
 
-        # self.wrd.add(3,3)
-        # self.assertDictEqual(self.wrd.count_vowels_consonants[['Algeria', 1894]], {'Algeria': {'vowels': 4, 'consonants': 3, 'message': ''}, 1894: {
-        #  'vowels': 0, 'consonants': 0, 'message': 'impout invalid'}}, "tests addition of two arguments on integers")
-
 
 if __name__ == '__main__':
     unittest.main()
